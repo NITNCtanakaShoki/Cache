bdaa70f4c086ae12b61df759a354dd335bae4ca8
tanakayouichi <tanakazoku@icloud.com>
2021-12-25 01:22:50 +0900

    DirectMappingAddressに振り分けメソッドを追加

--- src/main/cash/directMapping/models/DirectMappingAddress.java
+++ src/main/cash/directMapping/models/DirectMappingAddress.java
@@ -1,6 +1,9 @@
 package main.cash.directMapping.models;
 
+import main.cash.models.BlockCount;
 import main.models.Address;
+
+import java.util.Objects;
 
 public class DirectMappingAddress {
     private final Address address;
@@ -9,5 +12,24 @@ public class DirectMappingAddress {
     }
     public Integer block(int blocks, int words) {
         return address.asInteger() / words % blocks;
+    }
+    public static DirectMappingAddress create(int addressNumber) {
+        return new DirectMappingAddress(new Address(addressNumber));
+    }
+    public DirectMappingAddress representativeAddress(BlockCount count) {
+        return create(address.asInteger() / count.value());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DirectMappingAddress that = (DirectMappingAddress) o;
+        return address.equals(that.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(address);
     }
 }
--- src/test/cash/directMapping/models/DirectMappingAddressTest.java
+++ src/test/cash/directMapping/models/DirectMappingAddressTest.java
@@ -2,6 +2,7 @@ package test.cash.directMapping.models;
 import static org.junit.jupiter.api.Assertions.*;
 
 import main.cash.directMapping.models.DirectMappingAddress;
+import main.cash.models.BlockCount;
 import main.models.Address;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,16 @@ public class DirectMappingAddressTest {
         directMappingAddress = new DirectMappingAddress(new Address(1));
         assertEquals(0, directMappingAddress.block(4, 8));
 
+    }
+    @Test
+    public void createTest() {
+        var address = DirectMappingAddress.create(1234);
+    }
+
+    @Test
+    public void representativeAddressTest() {
+        var address = DirectMappingAddress.create(1235);
+        var represent = address.representativeAddress(new BlockCount(4));
+        assertEquals(DirectMappingAddress.create(308), represent);
     }
 }
